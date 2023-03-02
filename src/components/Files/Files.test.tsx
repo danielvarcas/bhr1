6398ecec53bb3e77f8ba35efe9797d178d99426a
Daniel Varcas <daniel.varcas@gmail.com>
2023-03-02 20:16:18 +0000

    add failing test for showing root level files/folders

---
+++ src/components/Files/Files.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import { Files } from "./Files"
+
+describe("Files", () => {
+  it("should display a list of root-level files and folders", () => {
+    render(<Files />)
+
+    expect(
+      screen.getByRole("listitem", { name: /Employee Handbook/i })
+    ).toBeVisible()
+
+    expect(screen.getAllByRole("listitem")).tohaveLength(5)
+  })
+})
