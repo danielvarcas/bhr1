25843173ad3ab860646a22fa10976d0e822d996f
Daniel Varcas <daniel.varcas@gmail.com>
2023-03-02 21:12:30 +0000

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { FileViewer } from "./components/FileViewer/FileViewer"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <h1>File Viewer</h1>
-      <FileViewer />
+      <Routes>
+        <Route path="/" element={<FileViewer />} />
+      </Routes>
     </div>
   )
 }
