04985c902e34254ce2d3eccd24f45db891387e9d
Daniel Varcas <daniel.varcas@gmail.com>
2023-03-02 20:05:28 +0000

    remove semi colons

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
 describe("App", () => {
   it("should render", () => {
-    render(<App />);
-    expect(screen.getByText("Hello world")).toBeVisible();
-  });
-});
+    render(<App />)
+    expect(screen.getByText("Hello world")).toBeVisible()
+  })
+})
